73ab045f123d1b1b4aa93077793fa708edcc857e
Aniket Tekawade <aniketkt@gmail.com>
2022-07-27 15:28:57 -0500

    re-organizing

--- scratchpad/polymer_am_eaton/bin/all_samples.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-python full_sample_porosity.py "TCRP3_AMTitan_200umlht"
-python full_sample_porosity.py "TCRP3_AMTitan_400umlht"
-python full_sample_porosity.py "TCRP3_AMcosine_400umlht"
-
-
